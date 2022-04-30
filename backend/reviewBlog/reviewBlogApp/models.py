7d131c853298ed4667d6897ef7554ddb545a83fd
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2022-04-29 17:07:09 -0700

    django backend API work. Create model, url paths and basic views logic

--- backend/reviewBlog/reviewBlogApp/models.py
+++ backend/reviewBlog/reviewBlogApp/models.py
@@ -1,3 +1,27 @@
+import email
 from django.db import models
+from django.forms import IntegerField
 
 # Create your models here.
+class User(models.Model):
+  email = models.EmailField(max_length=254)
+  username = models.CharField(max_length=30)
+  password = models.CharField(max_length=15)
+
+class Post(models.Model):
+  RATINGS_CHOICES = [
+    (0, 'Dont watch'),
+    (1, 'Bad'),
+    (2, 'Average'),
+    (3, 'Good'),
+    (4, 'Great'),
+    (5, 'Outstanding')
+  ]
+  movie_title = models.CharField(max_length=100)
+  post_date = models.DateField(auto_now_add=True)
+  rating = models.IntegerField(choices = RATINGS_CHOICES, default = 0)
+  review = models.TextField()
+  movie_image = models.URLField(max_length=1000)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
--- backend/reviewBlog/reviewBlogApp/views.py
+++ backend/reviewBlog/reviewBlogApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User, Post
 
 # Create your views here.
+def index(request):
+  print(Post)
+  return HttpResponse("Hello Elliot")
+
+def posts(request):
+  if request.method == "POST":
+    return HttpResponse("posts api create")
+  return HttpResponse("posts api")
